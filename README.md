19e9af7e8e39a674c69098ed482b67d8f92a4a8f
Olexiy Sokurenko <sokurenko-extern@gvl.de>
2024-01-04 01:42:12 +0100

    Add selenium testing setup and profile functionality tests
    Sucessfully implemented Selenium testing setup and added initial set of tests for profile functionality. Several new classes such as test fixtures, login, dashboard, and personal pages were created. Additionally, added an environment for storing sensitive data, and updated project configuration files.

---
+++ README.md
@@ -0,0 +1,13 @@
+# Selenium Demo on GitHub
+
+## Introduction
+
+## Project setup
+```
+# Install dependencies
+
+pip3 install selenium pytest webdriver-manager allure-pytest 
+
+# import requironments in requirements.txt
+pip3 freeze > requirements.txt 
+```  
---
+++ pages/personal_page.py
@@ -0,0 +1,27 @@
+import allure
+from base.base_page import BasePage
+from config.links import Links
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class PersonalPage(BasePage):
+    PAGE_URL = Links.PERSONAL_PAGE
+
+    FIRST_NAME_FIELD = ("xpath", "//input[@name='firstName']")
+    SAVE_BUTTON = ("xpath", "(//button[@type='submit'])[1]")
+
+    def change_name(self, new_name):
+        with allure.step(f"Change name to {new_name}"):
+            first_name_field = self.wait.until(EC.element_to_be_clickable(self.FIRST_NAME_FIELD))
+            first_name_field.clear()
+            assert first_name_field.get_attribute("value") == "", "There is text"
+            first_name_field.send_keys(new_name)
+            self.name = new_name
+
+    @allure.step("Save changes")
+    def save_changes(self):
+        self.wait.until(EC.element_to_be_clickable(self.SAVE_BUTTON)).click()
+
+    @allure.step("Changes have bin saved successfully")
+    def is_changes_saved(self):
+        self.wait.until(EC.text_to_be_present_in_element_value(self.FIRST_NAME_FIELD,self.name))
